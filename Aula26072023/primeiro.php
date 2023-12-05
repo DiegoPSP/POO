90fab097cc1f68d84465ed679e578c0fe0e01530
DiegoPSP <113466295+DiegoPSP@users.noreply.github.com>
2023-12-05 02:39:49 -0300

    Pasta com o primeiro arquivo em PHP
    Fiz esse código na minha primeira aula de POO na faculdade.

---
+++ Aula26072023/primeiro.php
@@ -0,0 +1,30 @@
+
+<?php
+
+	$olamundo = 'Olá, mundo!!';
+	echo 'Com aspas simples obtemos: $olamundo';
+?>
+<br/>
+
+
+<?php
+	echo "Com aspas duplas obtemos: $olamundo";
+
+
+Criando um array com a inicialização dos valores
+
+$listaNome = array("Diego Pereira", "Alan Nascimento", "Yuri da Silva");
+print("<pre>");
+print_r($listaNome);
+print("</pre>");
+
+Criando um array e inicializando seus valores individualmente em cada posição
+$listaTelefonica = array();
+$listaTelefonica [0] = "(33) 999946658";
+$listaTelefonica [1] = "(74) 981562438";
+$listaTelefonica [2] = "(74) 992345687";
+print("<pre>");
+print_r($listaTelefonica);
+print("</pre>");
+
+?>

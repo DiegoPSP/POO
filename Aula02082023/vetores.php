43028d8a905b8d0eb2d47eeff2b4e43e6636bb71
DiegoPSP <113466295+DiegoPSP@users.noreply.github.com>
2023-12-05 02:47:37 -0300

    Vetores em PHP
    Início de Vetores em PHP.

---
+++ Aula02082023/vetores.php
@@ -0,0 +1,29 @@
+<?php
+/*
+$dadosPessoais = array("Diego Pereira", "Pedro Bolfute", "Mano Deivinho");
+
+echo "<h1>Lista de Nomes</h1>";
+
+echo "<ul>";
+	foreach($dadosPessoais as $nome){
+		echo "<li>$nome</li>";
+	}
+echo "</ul>";
+*/
+
+define("CARRO", 'Fusca');
+echo "<h1>CARRO</h1>";
+
+$acessorios = array("Rodas de liga leve","DVD player","Ar condicionado");
+echo "<ul>";
+	foreach($acessorios as $valores){
+		echo "<li>$valores</li>";
+	}
+echo "</ul>";
+?>
+<html>
+<head>
+</head>
+<body>
+</body>
+</html>
